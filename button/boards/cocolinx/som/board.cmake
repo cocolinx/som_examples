15fb5d8688e6f0eb90b570b7bd086ec90ca7a14a
cocolinx <cocolinx@cocolinx.com>
2026-04-06 19:22:47 +0900

    Update project layout

--- button/boards/cocolinx/som/board.cmake
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2025 cocolinx
-# SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(jlink "--device=nRF9151_xxCA" "--speed=4000")
-include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
---
+++ examples/helloworld/src/main.c
@@ -0,0 +1,16 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h> 
+#include <modem/nrf_modem_lib.h>
+
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
+
+int main(void)
+{
+    int err;
+    err = nrf_modem_lib_init();
+    if(err < 0)
+        LOG_ERR("Unable to initialize modem lib. (err: %i)", err);
+    
+    LOG_INF("Hello World");
+    return 0;
+}
